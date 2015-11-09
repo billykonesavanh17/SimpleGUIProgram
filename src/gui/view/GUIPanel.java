8a5f74174de2a231a7ce6376e1f1295286e9e6bf
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-11-09 09:55:44 -0700

    Added a window builder editor and added springLayout.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,6 +3,7 @@ package gui.view;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout; //For layout
 import gui.controller.GUIController;
 
 /**
@@ -14,27 +15,37 @@ public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
 	private JButton firstButton;
-	private JTextField firstField;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
 		
-		firstButton = new JButton("Please do not click the button");
-		firstField = new JTextField("Words can be typed here");
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("Please do not click the button");	
+		firstTextField = new JTextField("Type here");
 		
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	
+	/**
+	 * Helper method to load all GUI components into the panel.
+	 */
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
 	}
 	
 	private void setupLayout()
 	{
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 118, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 102, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 0, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 169, SpringLayout.WEST, this);
 		
 	}
 	
