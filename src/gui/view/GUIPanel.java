8a4eb26130f71b93d7b1883750089432ca4d1005
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-11-09 09:40:08 -0700

    Added button, textfield and helper methods.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,20 +1,46 @@
 package gui.view;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import gui.controller.GUIController;
 
 /**
  * 
  * @author Billy Konesavanh
- * @version 0.1 November 9, 2015
+ * @version 0.2 November 9, 2015 added button, textfield and helper methods.
  */
 public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
+	private JButton firstButton;
+	private JTextField firstField;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
+		
+		firstButton = new JButton("Please do not click the button");
+		firstField = new JTextField("Words can be typed here");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
-
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
 }
